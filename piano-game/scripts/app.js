4e52cd2d7b396c14a308939871c47fa9b074a582
William Mayo <wlmayo@gmail.com>
2016-10-05 14:26:57 -0400

    different scales added. need to completely restructure the html and css of the keyboard

--- piano-game/scripts/app.js
+++ piano-game/scripts/app.js
@@ -13,20 +13,38 @@ $(document).ready(function(){
 const backgroundTest = $('body');
 backgroundTest.css('backgroundColor', 'green');
 
-const cMajor = ['c1', 'd1', 'e1', 'f1', 'g1', 'a1', 'b1', 'c2']
+
+// ARRAYS
+const cMajor = ['c1', 'd1', 'e1', 'f1', 'g1', 'a1', 'b1', 'c2'];
+const fMajor = ['f1', 'g1', 'a1', 'aSharp1', 'c2', 'd2', 'e2', 'f2'];
+const dMajor = ['d1', 'e1', 'f-sharp1', 'g1', 'a1', 'b1', 'c-sharp2', 'd2'];
 let playedScale = [];
-// Test Event Listenter
+let currentScale = cMajor;
+
+// EVENT LISTENERS
 $('.key').click(markCorrect);
-// test function
-// function playNote() {
-//   console.log(this);
-//   $(this).css('backgroundColor', 'blue');
-// }
+$('button').click(reset);
+
+
+// FUNCTIONS
+
+// generate new challenge
+function currentChallenge() {
+  const challenge = $('.challenge');
+  challenge.html("C Major Scale");
+}
+currentChallenge();
+
+
 
 let count = 0;
 function markCorrect() {
   // console.log ($(this).attr('id'));
-  if (($(this).attr('id')) === cMajor[count]) {
+
+
+
+
+  if ($(this).attr('id') === currentScale[count]) {
     playedScale.push($(this).attr('id'));
     $(this).css('backgroundColor', 'blue');
     count++;
@@ -37,9 +55,19 @@ function markCorrect() {
   console.log(playedScale)
 }
 
+function reset() {
+  playedScale = [];
+  $('.w-key').css('backgroundColor', 'white');
+  $('.b-key').css('backgroundColor', 'black');
+  count = 0
+}
+
 function checkScale() {
     if (count === 8) {
     alert('scale played correctly')
+    reset();
+    currentScale = dMajor;
+
   }
 }
 
